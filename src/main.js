42418c8a3771a7dc06f2a57590f8a93616e09dde
金科达 <12998598+jinkeda@user.noreply.gitee.com>
2023-10-09 21:23:42 +0800

    综合api+图片的替代

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import store from './store/store';
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render:h=>h(App),
+  store
+}).$mount('#app');
--- src/page/index/Swiper.vue
+++ src/page/index/Swiper.vue
@@ -1,18 +1,27 @@
 <template>
   <el-carousel :interval="4000" type="card" height="460px">
     <el-carousel-item v-for="item in imgs" :key="item">
-      <h3 class="medium"><img :src="require(`@/assets/${item}.png`)" alt=""></h3>
+      <h3 class="medium"><img :src="`${$store.state.url}/swiper/img/${item}`" alt=""></h3>
     </el-carousel-item>
   </el-carousel>
 </template>
 <script>
 import {Carousel,carouselItem} from 'element-ui'
+import axios from 'axios'
 export default {
 name:'Swiper',
 data(){
   return{
- imgs: ['1', '2', '3', '4', '5'], 
+ imgs: [], 
   }
+
+},
+created(){
+  axios.get(`${this.$store.state.url}/swiper`).then(res=>{
+    this.imgs = res.data
+  }).catch(err=>{
+    console.log(err);
+  })
 },
 components:{
   Carousel,
